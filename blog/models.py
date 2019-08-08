a431f4645438cf4e555b0c3fe0fcb30e7d8da542
Musyimi97 <collinsmusyimi.cm@gmail.com>
2019-08-08 11:49:13 +0300

    created an app and added models .its running fine

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,6 @@
 from django.db import models
-from django.contrib.auth.models import User
-
+from django.conf import settings
+User=settings.AUTH_USER_MODEL
 
 STATUS = (
     (0,"Draft"),
@@ -11,6 +11,7 @@ class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(User, on_delete= models.CASCADE,related_name='blog_posts')
+    cover = models.ImageField(upload_to='images/', default='')
     updated_on = models.DateTimeField(auto_now= True)
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
