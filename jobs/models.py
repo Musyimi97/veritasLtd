5c7652a35c249f5361ddf5f60b40fdfed9da8559
Musyimi97 <collinsmusyimi.cm@gmail.com>
2019-08-23 23:39:22 +0300

    Doing okay so far

--- jobs/models.py
+++ jobs/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from django.conf import settings
+User=settings.AUTH_USER_MODEL
 
-# Create your models here.
+STATUS = (
+    (0,"Draft"),
+    (1,"Publish")
+)
+J
+class Post(models.Model):
+    CHOICES = [
+        (0, 'Walk-ins'),
+        (1, 'BPO jobs'),
+        (2, 'Teaching jobs'),
+        (3, 'Diploma jobs'),
+        (4, 'Tech support'),
+        (5, 'Finance jobs'),
+        (6, 'Part time jobs'),
+        (7, 'Health care'),
+        (8, 'Hospitality'),
+        (9, 'Internships'),
+        (10, 'Research jobs'),
+        (11, 'Security jobs'),
+    ],
+    jobs = models.CharField(max_length=50, choices=CHOICES)
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(User, on_delete= models.CASCADE,related_name='blog_posts')
+    cover = models.ImageField(upload_to='images/', default='')
+    updated_on = models.DateTimeField(auto_now= True)
+    content = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return self.title
--- jobs/views.py
+++ jobs/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.views import generic
+from .models import Jobs
 
-# Create your views here.
+class JobsList(generic.ListView):
+    queryset = Jobs.objects.filter(status=1).order_by('-created_on')
+    template_name = 'veritas1/jobs.html'
+
+class JobsDetail(generic.DetailView):
+    model = Jobs
+    template_name = 'veritas1/location_single.html'
