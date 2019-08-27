d1b1c62d421e6563dda25a563fcc44b001ea7aef
Musyimi97 <collinsmusyimi.cm@gmail.com>
2019-08-27 22:33:06 +0300

    done with the  models

--- applications/models.py
+++ applications/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class Applications(models.Model):
+    CHOICES=(
+        (0, 'Employed'),
+        (1, 'Self_employed'),
+        (2, "Unemployed"),
+        (3, 'Student'),
+
+    )
+
+    name=models.CharField( max_length=50),
+    email_details=models.EmailField( max_length=254),
+    phone=models.PhoneNumberField(default=+254),
+    employment=models.CharField(choices=CHOICES, max_length=50, )
+
+    def __str__(self):
+        return 
+
+    def __unicode__(self):
+        return 
