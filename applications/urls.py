24405465065ae9c046753843963c0493d99fe3c9
Musyimi97 <collinsmusyimi.cm@gmail.com>
2019-08-28 00:28:18 +0300

    nagging...quite alot

--- applications/urls.py
+++ applications/urls.py
@@ -2,5 +2,5 @@ from django.urls import path,include
 from .views import model_form_upload
 
 urlpatterns = [
-    path('applications/', model_form_upload, name='resume')
+    path('applications/', model_form_upload, name='applications')
 ]
