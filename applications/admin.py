a146179e51effa096aebc68a20ac79d495ec44bd
Musyimi97 <collinsmusyimi.cm@gmail.com>
2019-09-04 22:16:32 +0300

    done this in a black better way

--- applications/admin.py
+++ applications/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from .models import Applications
 # Register your models here.
+class ApplicationsAdmin(admin.ModelAdmin):
+    list_display=('name', 'history', 'resume', 'phone')
+    list_filter=('resume',)
+    search_fields=['email', 'phone', 'name', ]
+
+
+admin.site.register(Applications,ApplicationsAdmin)
