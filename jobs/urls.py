c7f2030c382254c68c432ee82cc6b4683b33f844
Musyimi97 <collinsmusyimi.cm@gmail.com>
2019-09-20 21:26:02 +0300

    thats some progress

--- jobs/urls.py
+++ jobs/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path,include
 from jobs.views import JobsDetail, JobsList
+from . import views
 
 
 urlpatterns = [
     path('jobs',JobsList.as_view(),name='jobs' ),
     path('<slug:slug>/', JobsDetail.as_view(), name='jobs_detail'),
+    path('jobs/applications/', views.email, name='application')
 
 ]
--- jobs/views.py
+++ jobs/views.py
@@ -1,4 +1,14 @@
 from django.views import generic
+from django.core.files.storage import FileSystemStorage
+from django.core.mail import send_mail
+from .forms import ApplicationForm
+from django.core.mail import send_mail
+from django.core.mail import EmailMessage
+from django.conf import settings
+from django.utils.timezone import timezone
+from django.shortcuts import render
+
+
 from .models import Jobs
 
 class JobsList(generic.ListView):
@@ -8,3 +18,27 @@ class JobsList(generic.ListView):
 class JobsDetail(generic.DetailView):
     model = Jobs
     template_name = 'veritas1/jobs_details.html'
+
+
+
+def email(request):
+    if request.method == "POST":
+        form = ApplicationForm(request.POST, request.FILES)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.save()
+            email = request.POST.get('email')
+            subject = request.POST.get('subject')
+            message =request.POST.get('message')
+            document = request.POST.get('document')
+            email_from =settings.EMAIL_HOST__USER
+            recipient_list = [email]
+            email = EmailMessage(subject,message,email_from, recipient_list)
+            base_dir = 'media/documents/'
+            email.attach_file('media/documents/'+str(document))
+            email.send()
+    else:
+        form= ApplicationForm()
+        return render(request, 'veritas1/application.html', {'form':form})
+
+
