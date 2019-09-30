d9220e2669f8722538f5d23634cbf78baf56d34b
Musyimi97 <collinsmusyimi.cm@gmail.com>
2019-09-30 16:54:02 +0300

    splendid...veritas working fully with sendgrid

--- contactform/views.py
+++ contactform/views.py
@@ -15,7 +15,7 @@ def emailView(request):
             from_email = form.cleaned_data['from_email']
             message = form.cleaned_data['message']
             try:
-                send_mail(subject, message, from_email, ['admin@example.com'])
+                send_mail(subject, message, from_email, ['collinsmusyimi.cm@gmail.com'])
             except BadHeaderError:
                 return HttpResponse('Invalid header found.')
             return redirect('success')
