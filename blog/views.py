16eee3690f8cb89dc03c7bd038450c26fea47a6b
Musyimi97 <collinsmusyimi.cm@gmail.com>
2019-10-23 20:51:12 +0300

    pretty easy and fast done with the paginations of both events and jobs. Querryset is next

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,13 @@
 from django.views import generic
 from .models import Post
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
 
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1).order_by('-created_on')
     template_name = 'veritas1/events.html'
+    model =Post
+    paginate_by=4
 
 class PostDetail(generic.DetailView):
     model = Post
