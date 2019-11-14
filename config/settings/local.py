61f8f91c38aff71f12032319bcaa69a3c10383c5
labohkip81 <labankiplagat81@gmail.com>
2019-11-14 09:34:09 +0000

    added * to allowed hosts to pick all hosts

--- config/settings/local.py
+++ config/settings/local.py
@@ -11,7 +11,7 @@ SECRET_KEY = env(
     default="SepOiQv4tgo7DlDzc476kAPR6w2G5pwx0iCvvdSW1wwc0qevu5vengis9pbtzFwP",
 )
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1"]
+ALLOWED_HOSTS =['*']
 
 # CACHES
 # ------------------------------------------------------------------------------
